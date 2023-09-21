644272da3ce174e685f3733f975f76156fffe683
Chang-CH <chang.ch4348@gmail.com>
2023-09-21 15:54:06 +0800

    fix: throw array exceptions

--- src/components/ExecutionEngine/Interpreter/utils/instructions/stores.ts
+++ src/components/ExecutionEngine/Interpreter/utils/instructions/stores.ts
@@ -9,17 +9,17 @@ export function runIstore(thread: NativeThread, instruction: InstructionType) {
 }
 
 export function runLstore(thread: NativeThread, instruction: InstructionType) {
-  thread.storeLocal64(instruction.operands[0], thread.popStack());
+  thread.storeLocal64(instruction.operands[0], thread.popStack64());
   thread.offsetPc(2);
 }
 
 export function runFstore(thread: NativeThread, instruction: InstructionType) {
-  thread.storeLocal(instruction.operands[0], thread.popStack());
+  thread.storeLocal(instruction.operands[0], Math.fround(thread.popStack()));
   thread.offsetPc(2);
 }
 
 export function runDstore(thread: NativeThread, instruction: InstructionType) {
-  thread.storeLocal64(instruction.operands[0], thread.popStack64());
+  thread.storeLocal64(instruction.operands[0], thread.popStack64() + 1 - 1); // force into double
   thread.offsetPc(2);
 }
 
@@ -53,73 +53,73 @@ export function runIstore3(thread: NativeThread, instruction: InstructionType) {
 }
 
 export function runLstore0(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack();
+  const value = thread.popStack64();
   thread.storeLocal64(0, value);
   thread.offsetPc(1);
 }
 
 export function runLstore1(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack();
+  const value = thread.popStack64();
   thread.storeLocal64(1, value);
   thread.offsetPc(1);
 }
 
 export function runLstore2(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack();
+  const value = thread.popStack64();
   thread.storeLocal64(2, value);
   thread.offsetPc(1);
 }
 
 export function runLstore3(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack();
+  const value = thread.popStack64();
   thread.storeLocal64(3, value);
   thread.offsetPc(1);
 }
 
 export function runFstore0(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
-  thread.storeLocal(0, value);
+  thread.storeLocal(0, Math.fround(value));
   thread.offsetPc(1);
 }
 
 export function runFstore1(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
-  thread.storeLocal(1, value);
+  thread.storeLocal(1, Math.fround(value));
   thread.offsetPc(1);
 }
 
 export function runFstore2(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
-  thread.storeLocal(2, value);
+  thread.storeLocal(2, Math.fround(value));
   thread.offsetPc(1);
 }
 
 export function runFstore3(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
-  thread.storeLocal(3, value);
+  thread.storeLocal(3, Math.fround(value));
   thread.offsetPc(1);
 }
 
 export function runDstore0(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack64();
+  const value = thread.popStack64() + 1 - 1; // force into double
   thread.storeLocal64(0, value);
   thread.offsetPc(1);
 }
 
 export function runDstore1(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack64();
+  const value = thread.popStack64() + 1 - 1; // force into double
   thread.storeLocal64(1, value);
   thread.offsetPc(1);
 }
 
 export function runDstore2(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack64();
+  const value = thread.popStack64() + 1 - 1; // force into double
   thread.storeLocal64(2, value);
   thread.offsetPc(1);
 }
 
 export function runDstore3(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack64();
+  const value = thread.popStack64() + 1 - 1; // force into double
   thread.storeLocal64(3, value);
   thread.offsetPc(1);
 }
@@ -152,16 +152,36 @@ export function runIastore(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
   const index = thread.popStack();
   const arrayref = thread.popStack() as JavaArray;
-  console.warn('iastore: exceptions possibly not thrown');
+
+  if (arrayref === null) {
+    thread.throwNewException('java/lang/NullPointerException', '');
+    return;
+  }
+
+  if (index < 0 || index >= arrayref.len()) {
+    thread.throwNewException('java/lang/ArrayIndexOutOfBoundsException', '');
+    return;
+  }
+
   arrayref.set(index, value);
   thread.offsetPc(1);
 }
 
 export function runLastore(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack();
+  const value = thread.popStack64();
   const index = thread.popStack();
   const arrayref = thread.popStack() as JavaArray;
-  console.warn('lastore: exceptions possibly not thrown');
+
+  if (arrayref === null) {
+    thread.throwNewException('java/lang/NullPointerException', '');
+    return;
+  }
+
+  if (index < 0 || index >= arrayref.len()) {
+    thread.throwNewException('java/lang/ArrayIndexOutOfBoundsException', '');
+    return;
+  }
+
   arrayref.set(index, value);
   thread.offsetPc(1);
 }
@@ -170,16 +190,36 @@ export function runFastore(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
   const index = thread.popStack();
   const arrayref = thread.popStack() as JavaArray;
-  console.warn('fastore: exceptions possibly not thrown');
+
+  if (arrayref === null) {
+    thread.throwNewException('java/lang/NullPointerException', '');
+    return;
+  }
+
+  if (index < 0 || index >= arrayref.len()) {
+    thread.throwNewException('java/lang/ArrayIndexOutOfBoundsException', '');
+    return;
+  }
+
   arrayref.set(index, value);
   thread.offsetPc(1);
 }
 
 export function runDastore(thread: NativeThread, instruction: InstructionType) {
-  const value = thread.popStack();
+  const value = thread.popStack64();
   const index = thread.popStack();
   const arrayref = thread.popStack() as JavaArray;
-  console.warn('dastore: exceptions possibly not thrown');
+
+  if (arrayref === null) {
+    thread.throwNewException('java/lang/NullPointerException', '');
+    return;
+  }
+
+  if (index < 0 || index >= arrayref.len()) {
+    thread.throwNewException('java/lang/ArrayIndexOutOfBoundsException', '');
+    return;
+  }
+
   arrayref.set(index, value);
   thread.offsetPc(1);
 }
@@ -188,7 +228,17 @@ export function runAastore(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
   const index = thread.popStack();
   const arrayref = thread.popStack() as JavaArray;
-  console.warn('aastore: exceptions possibly not thrown');
+
+  if (arrayref === null) {
+    thread.throwNewException('java/lang/NullPointerException', '');
+    return;
+  }
+
+  if (index < 0 || index >= arrayref.len()) {
+    thread.throwNewException('java/lang/ArrayIndexOutOfBoundsException', '');
+    return;
+  }
+
   arrayref.set(index, value);
   thread.offsetPc(1);
 }
@@ -197,8 +247,18 @@ export function runBastore(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
   const index = thread.popStack();
   const arrayref = thread.popStack() as JavaArray;
-  console.warn('bastore: exceptions possibly not thrown');
-  arrayref.set(index, value);
+
+  if (arrayref === null) {
+    thread.throwNewException('java/lang/NullPointerException', '');
+    return;
+  }
+
+  if (index < 0 || index >= arrayref.len()) {
+    thread.throwNewException('java/lang/ArrayIndexOutOfBoundsException', '');
+    return;
+  }
+
+  arrayref.set(index, (value << 24) >> 24);
   thread.offsetPc(1);
 }
 
@@ -206,8 +266,18 @@ export function runCastore(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
   const index = thread.popStack();
   const arrayref = thread.popStack() as JavaArray;
-  console.warn('castore: exceptions possibly not thrown');
-  arrayref.set(index, value);
+
+  if (arrayref === null) {
+    thread.throwNewException('java/lang/NullPointerException', '');
+    return;
+  }
+
+  if (index < 0 || index >= arrayref.len()) {
+    thread.throwNewException('java/lang/ArrayIndexOutOfBoundsException', '');
+    return;
+  }
+
+  arrayref.set(index, value & 0xffff);
   thread.offsetPc(1);
 }
 
@@ -215,7 +285,17 @@ export function runSastore(thread: NativeThread, instruction: InstructionType) {
   const value = thread.popStack();
   const index = thread.popStack();
   const arrayref = thread.popStack() as JavaArray;
-  console.warn('sastore: exceptions possibly not thrown');
-  arrayref.set(index, value);
+
+  if (arrayref === null) {
+    thread.throwNewException('java/lang/NullPointerException', '');
+    return;
+  }
+
+  if (index < 0 || index >= arrayref.len()) {
+    thread.throwNewException('java/lang/ArrayIndexOutOfBoundsException', '');
+    return;
+  }
+
+  arrayref.set(index, (value << 16) >> 16);
   thread.offsetPc(1);
 }
