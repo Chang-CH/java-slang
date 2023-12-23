39eefb9add6d5106c8e8d535911a3ac74e245848
Chang-CH <chang.ch4348@gmail.com>
2023-12-23 10:26:33 +0800

    style: typescript eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,14 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: 'eslint:recommended',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'unused-imports',
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
